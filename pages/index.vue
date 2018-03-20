1519ab0858291b4b8080318f94ae1105da057085
WaqasArshad <waqasarshad777@gmail.com>
2018-03-20 20:16:29 +0500

    fixing blurb issue

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
             <div class="container">
                 <div class="col-sm-4 text-center" v-for="blurb in homeData.blurbs" :key="blurb.id">
                     <h3 style="margin-bottom:30px;">{{blurb.value}}</h3>
-                    <div class="img-circle h-300 w-300" style="`background: url(https://s3-us-west-2.amazonaws.com/cosmicjs/${blurb.children[0].value}}), background-size:cover, height: 300px, width: 300px`">
+                    <div class="img-circle h-300 w-300" :style="`background: url(https://s3-us-west-2.amazonaws.com/cosmicjs/${blurb.children[0].value}}), background-size:cover, height: 300px, width: 300px`">
                     </div>
                     <p style="margin-top:30px;">
                         {{blurb.children[1].value}}
